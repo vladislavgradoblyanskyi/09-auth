daf3afc09497c08faa834fe8abcdd103699061ab
vladislavgradoblyanskyi <vladislavgradoblyanskyi@gmail.com>
2026-06-28 21:22:42 +0200

    new try

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://09-auth-gray-psi.vercel.app//api"
 
 export const nextServer = axios.create({
   baseURL: BASE_URL,
